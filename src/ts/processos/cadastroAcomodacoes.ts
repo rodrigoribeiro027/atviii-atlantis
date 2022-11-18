a0b580efc67b952d390a90c187b12d2afc05b785
rodrigoribeiro027 <rodrigo.rsantos40@gmail.com>
2022-11-18 08:09:12 -0300

    Lista atviii

---
+++ src/ts/processos/cadastroAcomodacoes.ts
@@ -0,0 +1,37 @@
+import Processo from "../abstracoes/processo";
+import DiretorCasalSimples from "../diretores/diretorCasalSimples";
+import DiretorFamilaSimples from "../diretores/diretorFamiliaSimples";
+import DiretorFamiliaSuper from "../diretores/diretorFamiliaSuper";
+import DiretorFamíliaMais from "../diretores/diretorFamíliaMais";
+import DiretorSolteiroMais from "../diretores/diretorSolteiroMais";
+import DiretorSolteiroSimples from "../diretores/diretorSolteiroSimples";
+import Armazem from "../dominio/armazem";
+import Acomodacao from "../modelos/acomodacao";
+
+export default class CadastroAcomodacoes extends Processo {
+    private acomodacoes: Acomodacao[]
+    constructor() {
+        super()
+        this.acomodacoes = Armazem.InstanciaUnica.Acomodacoes
+    }
+    processar(): void {
+        let diretor = new DiretorSolteiroSimples()
+        this.acomodacoes.push(diretor.construir())
+        
+        let diretor2 = new DiretorCasalSimples()
+        this.acomodacoes.push(diretor2.construir())
+        
+        let diretor3 = new DiretorFamilaSimples()
+        this.acomodacoes.push(diretor3.construir())
+
+        let diretor4 = new DiretorFamiliaSuper()
+        this.acomodacoes.push(diretor4.construir())
+
+        let diretor5 = new DiretorFamíliaMais()
+        this.acomodacoes.push(diretor5.construir())
+
+        let diretor6 = new DiretorSolteiroMais()
+        this.acomodacoes.push(diretor6.construir())
+        
+    }
+}
